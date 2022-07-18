a4ce5672948b4e8337859eb2b29269e68d53db77
barneyman <barnaby.james.flint@gmail.com>
2022-07-18 08:05:12 +0000

    lint

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@
 
 import voluptuous as vol
 import logging
+
 _LOGGER = logging.getLogger(__name__)
 
 # The domain of your component. Should be equal to the name of your component.
@@ -12,9 +13,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
 import homeassistant.helpers.config_validation as cv
 
-offSchema=vol.Schema({
-    vol.Required("at"): cv.time
-})
+offSchema = vol.Schema({vol.Required("at"): cv.time})
 
 # TODO enable checking
 CONFIG_SCHEMA = vol.Schema(
@@ -22,50 +21,65 @@ CONFIG_SCHEMA = vol.Schema(
         DOMAIN: vol.Schema(
             {
                 vol.Required("tempsensor"): cv.string,
-                vol.Required("threshold"): vol.All(vol.Coerce(float), vol.Range(min=0, max=100)),
-
+                vol.Required("threshold"): vol.All(
+                    vol.Coerce(float), vol.Range(min=0, max=100)
+                ),
                 vol.Required("instances"): vol.All(
                     cv.ensure_list,
                     [
                         {
-                            vol.Required("name"): cv.string,  
-                            vol.Required("azimuth"): vol.Schema({
-                                vol.Required("min"): vol.All(vol.Coerce(float), vol.Range(min=0, max=359)),
-                                vol.Required("max"): vol.All(vol.Coerce(float), vol.Range(min=0, max=359))
-                            }),
-                            vol.Optional("elevation"): vol.Schema({
-                                vol.Optional("min"): vol.All(vol.Coerce(float), vol.Range(min=0, max=90)),
-                                vol.Optional("max"): vol.All(vol.Coerce(float), vol.Range(min=0, max=90))
-                            }),
-
+                            vol.Required("name"): cv.string,
+                            vol.Required("azimuth"): vol.Schema(
+                                {
+                                    vol.Required("min"): vol.All(
+                                        vol.Coerce(float), vol.Range(min=0, max=359)
+                                    ),
+                                    vol.Required("max"): vol.All(
+                                        vol.Coerce(float), vol.Range(min=0, max=359)
+                                    ),
+                                }
+                            ),
+                            vol.Optional("elevation"): vol.Schema(
+                                {
+                                    vol.Optional("min"): vol.All(
+                                        vol.Coerce(float), vol.Range(min=0, max=90)
+                                    ),
+                                    vol.Optional("max"): vol.All(
+                                        vol.Coerce(float), vol.Range(min=0, max=90)
+                                    ),
+                                }
+                            ),
                         }
-                    ]
-                )
+                    ],
+                ),
             }
         )
-    }, extra=vol.ALLOW_EXTRA
+    },
+    extra=vol.ALLOW_EXTRA,
 )
 
-suninstances=[]
+suninstances = []
 
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Your controller/hub specific code."""
 
-    _LOGGER.info("{} async_setup {}".format(DOMAIN, config))
+    _LOGGER.info("%s async_setup %s", DOMAIN, config)
 
     if DOMAIN in config:
-        
-        myConfig=config.get(DOMAIN)
 
-        hass.data[DOMAIN]=myConfig
+        myConfig = config.get(DOMAIN)
 
-        _LOGGER.info("{} config {}".format(DOMAIN, myConfig))
+        hass.data[DOMAIN] = myConfig
+
+        _LOGGER.info("%s config %s", DOMAIN, myConfig)
 
         # see hass.helpers.discovery.async_load_platform for the create task
-        hass.async_create_task(hass.helpers.discovery.async_load_platform('sensor', DOMAIN, None, config))
+        hass.async_create_task(
+            hass.helpers.discovery.async_load_platform("sensor", DOMAIN, None, config)
+        )
 
     else:
-        _LOGGER.error("No config for {}".format(DOMAIN))
+        _LOGGER.error("No config for %s", (DOMAIN))
 
     return True
--- sensor.py
+++ sensor.py
@@ -1,5 +1,5 @@
 from homeassistant.components.sensor import SensorEntity
-from homeassistant.const import TEMP_CELSIUS, STATE_UNKNOWN, STATE_UNAVAILABLE
+from homeassistant.const import STATE_UNKNOWN, STATE_UNAVAILABLE
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
@@ -7,6 +7,7 @@ from homeassistant.components.template.sensor import SensorTemplate
 from homeassistant.helpers.template import Template
 
 import logging
+
 _LOGGER = logging.getLogger(__name__)
 
 # The domain of your component. Should be equal to the name of your component.
@@ -17,152 +18,177 @@ async def async_setup_platform(
     hass: HomeAssistant,
     config: ConfigType,
     add_entities: AddEntitiesCallback,
-    discovery_info: DiscoveryInfoType
+    discovery_info: DiscoveryInfoType,
 ) -> None:
 
-    _LOGGER.info("async_setup_platform! sunload {} {}".format(config,discovery_info))
+    _LOGGER.info("async_setup_platform! sunload %s %s", config, discovery_info)
 
-    """Set up the sensor platform."""
+    # """Set up the sensor platform."""
 
     # config isn't passed from async_setup() [or i'm doing something wrong!]
     # so grab a cached copy from dad
-    config=hass.data[DOMAIN]
+    config = hass.data[DOMAIN]
 
-    _LOGGER.info("hass.config.as_dict {}".format(config))
+    _LOGGER.info("hass.config.as_dict %s", (config))
 
-
-    rewiredSensors=[]
+    rewiredSensors = []
 
     # first - turn the sun.sun attributes we care about into sensors
     _LOGGER.info("Adding azimuth sensor")
-    sensorConfig={"name":Template("sunload_azimuth"),"unit_of_measurement":"°", "state_class": "measurement","state":Template("{{ state_attr('sun.sun', 'azimuth') }}")}
-    rewiredSensors.append(SensorTemplate(hass,sensorConfig,"sunload.internal.azimuth" ))
+    sensorConfig = {
+        "name": Template("sunload_azimuth"),
+        "unit_of_measurement": "°",
+        "state_class": "measurement",
+        "state": Template("{{ state_attr('sun.sun', 'azimuth') }}"),
+    }
+    rewiredSensors.append(
+        SensorTemplate(hass, sensorConfig, "sunload.internal.azimuth")
+    )
 
     _LOGGER.info("Adding elevation sensor")
-    sensorConfig={"name":Template("sunload_elevation"),"unit_of_measurement":"°", "state_class": "measurement","state":Template("{{ state_attr('sun.sun', 'elevation') }}")}
-    rewiredSensors.append(SensorTemplate(hass,sensorConfig, "sunload.internal.elevation" ))
+    sensorConfig = {
+        "name": Template("sunload_elevation"),
+        "unit_of_measurement": "°",
+        "state_class": "measurement",
+        "state": Template("{{ state_attr('sun.sun', 'elevation') }}"),
+    }
+    rewiredSensors.append(
+        SensorTemplate(hass, sensorConfig, "sunload.internal.elevation")
+    )
 
     add_entities(rewiredSensors)
 
     # walk thru the config
-    newSensors=[]
+    newSensors = []
 
-    tempSensor=config["tempsensor"]
-    threshold=config["threshold"]
+    tempSensor = config["tempsensor"]
+    threshold = config["threshold"]
 
     if "instances" in config:
 
         for instance in config["instances"]:
-            newinstance=sunloadInstance(hass,instance, tempSensor, threshold)
-            _LOGGER.info("adding instance {}".format(newinstance.name))
+            newinstance = sunloadInstance(hass, instance, tempSensor, threshold)
+            _LOGGER.info("adding instance %s", (newinstance.name))
             newSensors.append(newinstance)
 
         add_entities(newSensors)
 
     else:
-        _LOGGER.error("No instances configured for {}".format(DOMAIN))
+        _LOGGER.error("No instances configured for %s", (DOMAIN))
 
 
 class sunloadInstance(SensorEntity):
-
     def __init__(self, hass, config, tempsensor, threshold) -> None:
-        
-        _LOGGER.info("sunloadInstance init {}".format(config))
 
-        self._name="{}_{}".format(DOMAIN,config["name"])
-        self._state=None
-        self._hass=hass
+        _LOGGER.info("sunloadInstance init %s", (config))
 
-        self._inAzimuth=None
-        self._inElevation=None
+        self._name = "{}_{}".format(DOMAIN, config["name"])
+        self._state = None
+        self._hass = hass
 
-        self._tempsensor=tempsensor
-        self._threshold=threshold
+        self._inAzimuth = None
+        self._inElevation = None
+
+        self._tempsensor = tempsensor
+        self._threshold = threshold
 
         # get the deets
         # azimuth
-        azimuth=config["azimuth"]
-        self._azimuth_min=azimuth["min"]
-        self._azimuth_max=azimuth["max"]
+        azimuth = config["azimuth"]
+        self._azimuth_min = azimuth["min"]
+        self._azimuth_max = azimuth["max"]
 
-        self._elevation_min=self._elevation_max=None
+        self._elevation_min = self._elevation_max = None
 
         if "elevation" in config:
             if "min" in config["elevation"]:
-                self._elevation_min=config["elevation"]["min"]
+                self._elevation_min = config["elevation"]["min"]
 
             if "max" in config["elevation"]:
-                self._elevation_max=config["elevation"]["max"]
+                self._elevation_max = config["elevation"]["max"]
 
         # and update
         self.update()
 
     def update(self):
-        _LOGGER.debug("update! {}".format(self._name))
+        _LOGGER.debug("update! %s", (self._name))
 
         # get el and az
-        az=self._hass.states.get("sensor.sunload_azimuth")
-        el=self._hass.states.get("sensor.sunload_elevation")
+        az = self._hass.states.get("sensor.sunload_azimuth")
+        el = self._hass.states.get("sensor.sunload_elevation")
 
-        if az is None or el is None or az.state==STATE_UNKNOWN or el.state==STATE_UNKNOWN:
+        if (
+            az is None
+            or el is None
+            or az.state == STATE_UNKNOWN
+            or el.state == STATE_UNKNOWN
+        ):
             return
 
+        az = float(az.state)
+        el = float(el.state)
 
-        az=float(az.state)
-        el=float(el.state)
+        _LOGGER.info("%s az %f el %f", self._name, az, el)
 
-        _LOGGER.info("{} az {} el {}".format(self._name,az,el))
-
-        wrapFix=0
-        self._inAzimuth=False
+        wrapFix = 0
+        self._inAzimuth = False
 
         # have to spot crossing thr 0/360 line
         if self._azimuth_max > self._azimuth_min:
             # has the potential to cross the border, so just rotate the world, consistently
-            wrapFix=(360-self._azimuth_max)
+            wrapFix = 360 - self._azimuth_max
 
-            _LOGGER.info("{} wrapping az {} min {} with {}".format(self._name, az, self._azimuth_min, wrapFix))
+            _LOGGER.info(
+                "%s wrapping az %f min %f with %f",
+                self._name,
+                az,
+                self._azimuth_min,
+                wrapFix,
+            )
 
-            if (az+wrapFix)%360 <  self._azimuth_min+wrapFix:
-                self._inAzimuth=True
-                
+            if (az + wrapFix) % 360 < self._azimuth_min + wrapFix:
+                self._inAzimuth = True
+
         elif az < self._azimuth_min and az > self._azimuth_max:
-                self._inAzimuth=True
+            self._inAzimuth = True
 
         # elevation
-        self._inElevation=True
+        self._inElevation = True
 
         if self._elevation_min is not None:
             if el < self._elevation_min:
-                self._inElevation=False
+                self._inElevation = False
 
         if self._elevation_max is not None:
             if el > self._elevation_max:
-                self._inElevation=False
-        
+                self._inElevation = False
+
         # ok - now check the temp
-        ctemp=self._hass.states.get(self._tempsensor)
+        ctemp = self._hass.states.get(self._tempsensor)
 
         if ctemp is not None and ctemp.state not in [STATE_UNKNOWN, STATE_UNAVAILABLE]:
 
-            self._state=False
-            if self._inElevation and self._inAzimuth and float(ctemp.state)>self._threshold:
-                self._state=True
+            self._state = False
+            if (
+                self._inElevation
+                and self._inAzimuth
+                and float(ctemp.state) > self._threshold
+            ):
+                self._state = True
 
-            _LOGGER.info("{} state is {}".format(self._name, self._state))
-
+            _LOGGER.info("%s state is %s", self._name, self._state)
 
     @property
     def name(self) -> str:
         """Return the name of the sensor."""
         return self._name
 
-    @property
-    def state(self):
-        """Return the state of the sensor."""
-        return self._state
+    # @property
+    # def state(self):
+    #     """Return the state of the sensor."""
+    #     return self._state
 
     @property
-    def extra_state_attributes(self): #-> Mapping[str, Any] | None:
+    def extra_state_attributes(self):  # -> Mapping[str, Any] | None:
 
-        return {"inazimuth":self._inAzimuth,"inelevation":self._inElevation }
+        return {"inazimuth": self._inAzimuth, "inelevation": self._inElevation}
